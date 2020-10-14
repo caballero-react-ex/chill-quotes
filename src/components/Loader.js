da29bf286524d3a0030dc14a2c9b348a70dc0f96
Luis Caballero <mendoza386@gmail.com>
2020-10-14 11:22:10 +0200

    loader component + css created

---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Loader.scss';
+
+function Loader() {
+  return (
+    <div className="loader">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  )
+}
+
+export default Loader; 
