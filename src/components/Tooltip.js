74e882e85ae05d702e7657a5c7bee9c0c5d96035
Luis Caballero <mendoza386@gmail.com>
2020-10-14 16:12:24 +0200

    tootip proptypes

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Tooltip.scss';
 
 function Tooltip({children}) {
@@ -11,3 +12,7 @@ function Tooltip({children}) {
 }
 
 export default Tooltip; 
+
+Tooltip.propTypes = {
+  children: PropTypes.string.isRequired,
+}
