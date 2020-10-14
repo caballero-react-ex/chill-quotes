65b292e5b811eceaf10b7e8e905439e9ca0caed9
Luis Caballero <mendoza386@gmail.com>
2020-10-14 19:35:42 +0200

    Updates

---
+++ precache-manifest.3be0f68ab3cc02d34230a7a96a341b2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741aa349348432c4858dd06053122b6b",
+    "url": "/chillquotes/index.html"
+  },
+  {
+    "revision": "cf292e3534825bdc02d4",
+    "url": "/chillquotes/static/css/main.2bd54f46.chunk.css"
+  },
+  {
+    "revision": "7667d86f5ab4e120c135",
+    "url": "/chillquotes/static/js/2.ee297094.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chillquotes/static/js/2.ee297094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf292e3534825bdc02d4",
+    "url": "/chillquotes/static/js/main.530c0f33.chunk.js"
+  },
+  {
+    "revision": "8097b37d4e26b977afdb",
+    "url": "/chillquotes/static/js/runtime-main.90107953.js"
+  },
+  {
+    "revision": "fc5eb2fdd44b024e1a93ba270c8e93d1",
+    "url": "/chillquotes/static/media/logo-icon.fc5eb2fd.svg"
+  },
+  {
+    "revision": "86539e00eced9e4288064d88b47b647b",
+    "url": "/chillquotes/static/media/quotes-icon.86539e00.svg"
+  }
+]);
